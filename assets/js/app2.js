771df733a17ea5a8861dd72d53b67b1dc2f92c3a
jye <github@crossingmoments.com>
2015-06-07 00:53:48 -0700

    Updated js

---
+++ assets/js/app2.js
@@ -0,0 +1,77 @@
+$( document ).ready(function() {
+
+    var getDroughtData = function() {
+        $.ajax({
+            url: "data/drought.json",
+            type: "GET",
+            dataType: "json",
+            async: false
+        }).success(function(data) {
+            return data.respsonseJSON;
+        }).error(function() {
+            return [];
+        });
+    };
+
+    var renderData = function() {
+        $.ajax({
+            url: "data/_index.json",
+            type: "GET",
+            dataType: "json"
+        }).success(function(charts) {
+            for (var i=0; i < charts.length; i++) {
+                $.ajax({
+                    url: "data/" + charts[i].file,
+                    type: "GET",
+                    dataType: "json"
+                }).success(function(chart) {
+                    addGraph(chart, "chart" + i);
+                });
+            }
+        });
+    };
+
+    var addGraph = function(data, id) {
+        nv.addGraph(function () {
+            var chart = nv.models.cumulativeLineChart()
+                .useInteractiveGuideline(true)
+                .x(function (d) {
+                    return d[0];
+                })
+                .y(function (d) {
+                    return d[1] / 100;
+                })
+                .color(d3.scale.category10().range())
+                .duration(300)
+                .clipVoronoi(false);
+            chart.dispatch.on('renderEnd', function () {
+                console.log('render complete: cumulative line with guide line');
+            });
+
+            chart.xAxis.tickFormat(function (d) {
+                return d3.time.format('%m/%d/%y')(new Date(d))
+            });
+
+            chart.yAxis.tickFormat(d3.format(''));
+
+            d3.select("#charts div:first-child")
+                .append("svg")
+                .datum(data)
+                .call(chart);
+
+            nv.utils.windowResize(chart.update);
+
+            chart.dispatch.on('stateChange', function (e) {
+                nv.log('New State:', JSON.stringify(e));
+            });
+            chart.state.dispatch.on('change', function (state) {
+                nv.log('state', JSON.stringify(state));
+            });
+
+            return chart;
+        });
+    };
+
+    // Generate all the charts
+    renderData();
+});
